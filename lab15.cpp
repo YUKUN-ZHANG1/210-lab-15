1a0641721980a391860605c8127ad246c69c4bd6
YUKUN-ZHANG1 <137257662+YUKUN-ZHANG1@users.noreply.github.com>
2024-09-29 19:56:54 -0700

    finished class

--- lab15.cpp
+++ lab15.cpp
@@ -1,2 +1,30 @@
-// COMSC-210 | Lab 15: Color Class | Yukun Zhang
+// COMSC-210 | Lab 15: Movie Class | Yukun Zhang
 // IDE used: Visual Studio Code
+
+#include <iostream>
+#include <iomanip>
+#include <vector> 
+#include <fstream> 
+using namespace std; 
+
+class Movie{
+    private: 
+    int yearReleased;
+    string screenWriter, title;
+    public:
+    Movie(string screenWriter, int yearReleased, string title)
+        :screenWriter(screenWriter), yearReleased(yearReleased), title(title){}
+    //Setter and getter
+    void setYearReleased(int yearReleased){this->yearReleased = yearReleased;}
+    void setScreenWriter(string screenWriter){this->screenWriter = screenWriter;}
+    void setTitle(string title){this->title = title;}
+    int getYearReleased(){return yearReleased;}
+    string getScreenWriter(){return screenWriter;}
+    string getTitle(){return title;}
+
+    void print(){
+        cout<<"Movie: "<<title<<endl;
+        cout<<"    Year released: "<<yearReleased<<endl;
+        cout<<"    Screenwriter: "<<screenWriter<<endl;
+    }
+};
