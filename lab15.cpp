2c82d17dd4e828fda7d551581f000394938328f5
YUKUN-ZHANG1 <137257662+YUKUN-ZHANG1@users.noreply.github.com>
2024-09-29 20:08:28 -0700

    finished main

--- lab15.cpp
+++ lab15.cpp
@@ -18,13 +18,41 @@ class Movie{
     void setYearReleased(int yearReleased){this->yearReleased = yearReleased;}
     void setScreenWriter(string screenWriter){this->screenWriter = screenWriter;}
     void setTitle(string title){this->title = title;}
-    int getYearReleased(){return yearReleased;}
-    string getScreenWriter(){return screenWriter;}
-    string getTitle(){return title;}
+    int getYearReleased()const{return yearReleased;}
+    string getScreenWriter()const{return screenWriter;}
+    string getTitle()const{return title;}
 
-    void print(){
+    void print()const{
         cout<<"Movie: "<<title<<endl;
         cout<<"    Year released: "<<yearReleased<<endl;
         cout<<"    Screenwriter: "<<screenWriter<<endl;
     }
 };
+
+int main(){
+    vector<Movie> movies; // Vector to store Movie objects
+    ifstream inputFile("movies.txt");
+    if (!inputFile) {
+        cerr << "Error: Unable to open file." << endl;
+        return 1;
+    }
+    string title, screenWriter;
+    int yearReleased;
+
+    // Reading data from file and storing in vector
+    while (getline(inputFile, title)) {
+        inputFile >> yearReleased;
+        inputFile.ignore(); // Ignore the newline after the year
+        getline(inputFile, screenWriter);
+        movies.push_back(Movie(screenWriter, yearReleased, title)); // Store movie object
+    }
+    inputFile.close();
+
+    // Output the contents of the vector
+    for (const auto& movie : movies) {
+        movie.print();
+        cout << endl;
+    }
+
+    return 0;
+}
